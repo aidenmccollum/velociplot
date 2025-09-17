9ea8263be263b499c3fa5094afd832ce51e372e9
Aiden McCollum <aidenjmccollum@gmail.com>
2025-09-17 15:29:12 -0400

    Initializing the app with a barebones svelte app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<!-- this is the home page. the root URL will lead here -->
+<h1>Welcome to SvelteKit</h1>
+<p>
+    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+    documentation
+</p>
