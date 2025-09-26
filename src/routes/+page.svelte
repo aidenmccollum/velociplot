28e8009a648722c884dbfbe6661876ae4d23c2dd
Aiden McCollum <aidenjmccollum@gmail.com>
2025-09-26 15:28:14 -0400

    heres my new paragraph

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,15 @@
 <!-- this is the home page. the root URL will lead here -->
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-    documentation
-</p>
+
+
+<div >
+    <h1>Welcome to SvelteKit</h1>
+    <p>
+        Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+        documentation
+    </p>
+
+    <p>
+        This is my new paragraph
+    </p>
+
+</div>
